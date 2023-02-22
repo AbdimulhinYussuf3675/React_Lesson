480f738a26d814dc6023a66605203ab89307f186
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-02-22 21:47:11 +0300

    Update for jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-  ]);
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" animal="dog" breed="Havanese" />
+    <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+    <Pet name="Doink" animal="cat" breed="Mix" />
+  </div>;
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
