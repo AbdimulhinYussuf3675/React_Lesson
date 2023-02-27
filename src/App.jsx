88c4e6de1bbf1bd6637f9c7f4f728ab7ba0a6580
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-02-27 16:49:08 +0300

    Add react core concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </StrictMode>
   );
 };
 
