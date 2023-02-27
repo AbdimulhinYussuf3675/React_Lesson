c6db9a3c569592f167bd869cbee743506ba6e673
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-02-27 13:54:15 +0300

    Add user form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { createRoot } from "react-dom/client";
-import Pet from "./Pet";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Luna" animal="dog" breed="Havanese" />
-    <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-    <Pet name="Doink" animal="cat" breed="Mix" />
-  </div>;
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
--- src/Details.jsx
+++
@@ -1,5 +0,0 @@
-const Details = () => {
-    return <h2>hi!</h2>;
-  };
-  
-  export default Details;
