9d0e764bc0ea301ca9052082763732cb077010a6
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-02-27 17:39:43 +0300

    Add react Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { StrictMode } from "react";
+import { Link, BrowserRouter, Route, Routes } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <StrictMode>
-      <div>
-        <h1>Adopt Me!</h1>
-        <SearchParams />
-      </div>
-    </StrictMode>
+    <BrowserRouter>
+      <StrictMode>
+        <header>
+          <Link to="/">Adopt Me</Link>
+        </header>
+          <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </StrictMode>
+    </BrowserRouter>
   );
 };
 
